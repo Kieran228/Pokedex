dd890ea4e4925eb2d9fb91f6e4653024ad70f01a
Kieran Caraway <kierancaraway20@yahoo.com>
2024-10-03 16:04:03 -0500

    latest update
    added a canvas in html. added a div  for the left side of the pokedex display and a div for the right side of the pokedex display.

---
+++ js/pokedex.js
@@ -0,0 +1,10 @@
+const pokemonCount = 151;
+let pokedex = {}; //? creating a pokedex map. the key will be the numbers from 1-151. {"name", : "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."}
+
+window.onload = function() {
+    getPokemon(1);
+}
+
+function getPokemon(num) {
+    let url = ""
+}
