a8ca4944a3c0a4d37069b30bd8edd146a439efe7
Kieran Caraway <kierancaraway20@yahoo.com>
2024-10-03 22:41:17 -0500

    pokemon list
    added a pokemon list by iterating through the pokemon array, then while iterating through each pokemon creating a new div for each pokemon. then I am adding the class name "pokemon-name". after that i am creating a node for the pokemon list, and appending each pokemon to that list.

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,11 +1,20 @@
 const pokemonCount = 151;
 let pokedex = {}; //? creating a pokedex map. the key will be the numbers from 1-151. {"name", : "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."}
 
+//todo added a pokemon list by iterating through the pokemon array, then while iterating through each pokemon creating a new div for each pokemon. then I am adding the class name "pokemon-name". after that i am creating a node for the pokemon list, and appending each pokemon to that list.
+
 window.onload = async function() {
     // getPokemon(1);
 
     for (let i = 1; i <= pokemonCount; i++) {
         await getPokemon(i)
+
+        //? <div id="1" class="pokemon-name">Bulbasaur</div>
+        let pokemon = document.createElement("div");
+        pokemon.id = i;
+        pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+        pokemon.classList.add("pokemon-name");
+        document.getElementById("pokemon-list").append(pokemon);
     }
 
     console.log(pokedex)
