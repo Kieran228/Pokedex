5e24cecae9791de2d0d1d41ba4e1a93a0cea00e7
Kieran Caraway <kierancaraway20@yahoo.com>
2024-10-03 22:09:08 -0500

    latest update
    added a getPokemon function that uses the pokemon api to fetch data. i declared variables that were equal to different attributes of pokemon.  i then set the pokedex variable equal to a map of all the attributes that were fetched. we then iterate through the array of all the pokemon and call the get pokemon function within the for loop.

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,10 +1,32 @@
 const pokemonCount = 151;
 let pokedex = {}; //? creating a pokedex map. the key will be the numbers from 1-151. {"name", : "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."}
 
-window.onload = function() {
-    getPokemon(1);
+window.onload = async function() {
+    // getPokemon(1);
+
+    for (let i = 1; i <= pokemonCount; i++) {
+        await getPokemon(i)
+    }
+
+    console.log(pokedex)
 }
 
-function getPokemon(num) {
-    let url = ""
+async function getPokemon(num) {
+    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+
+    let response = await fetch(url);
+    let pokemon = await response.json();
+    // console.log(pokemon);
+
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"];
+    let pokemonIMG = pokemon["sprites"]["front_shiny"];
+
+    response = await fetch(pokemon["species"]["url"]);
+    let pokemonDescription = await response.json();
+
+    pokemonDescription = pokemonDescription["flavor_text_entries"][8]["flavor_text"];
+
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonIMG, "types" : pokemonType, "description" : pokemonDescription};
+    // console.log(pokemonDescription)
 }
